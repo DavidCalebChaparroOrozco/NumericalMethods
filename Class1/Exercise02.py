0e4a58662637a712c6d6e1793e4da030e77dd06b
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-02-09 15:40:41 -0500

    Ejercicio 2: Cree variables que contenga nombre, carrera y semestre actual

---
+++ Class1/Exercise02.py
@@ -0,0 +1,15 @@
+# Ejercicio 2:
+#     Cree una variable tipo string que contenga su nombre (var1s)
+#     Cree una segunda variable tipo string que contenga el programa en el que está matriculado (var2s)
+#     Cree una variable tipo entera que contenga el semestre que cursa en su programa (var3n)
+#     Concatene las variables de tres formas diferentes:
+#         Forma 1: "Mi nombre es " + var1s + ". Soy estudiante de " + var2s + " y estoy en el semestre " + var3n
+#         Forma 2: "Mi nombre es {}. Soy estudiante de {} y estoy en el semestre {}".format(var1s,var2s,var3n)
+#         Forma 3: f"Mi nombre es {var1s}. Soy estudiante de {var2s} y estoy en el semestre {var3n}"
+#     Imprima cada variable y las concatenaciones construidas
+
+var1s = 'David Caleb'
+var2s = 'Software Engineer'
+var3s = 9
+print(f'Name: {var1s}, Career: {var2s}, Semester: {var3s}')
+print("End exercise 2 ".center(50,"*"))
