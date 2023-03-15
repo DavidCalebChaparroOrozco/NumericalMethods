979d95ae4f77e548026b4e640a560a7ae949b34d
David Caleb Chaparro Orozco <davidcaleb1998@hotmail.com>
2023-03-15 07:55:24 -0500

    Problemas con Valores en la Frontera (PVF)

--- Class10/S4_C1_PVI_Solucion.py
+++ Class10/S4_C1_PVI_Solucion.py
@@ -1,5 +1,67 @@
+# '''
+# Código construido para solucionar un PVIs conformado por una EDO de primer orden de la forma y'=fld(t,y) y condiciones iniciales y(t_0)=y_0.
+# TAREA: Agregar líneas comentadas que faciliten la comprensión del código.
+# '''
+
+# import numpy as np
+# import pvi_methods as pvim
+# import matplotlib.pyplot as plt
+
+# # Definición de la función f (lado derecho de la EDO)
+# def fld(t, y_1,y_2):
+#     return y_2
+
+# def fld2(t, y_1, y_2):
+#     return -0.6*y_2 -8*y_1
+
+# # Solución analítica del PVI
+# def fanalit(t):
+#     y = np.exp(t)
+#     return y
+
+# # Información de entrada
+# # Condiciones iniciales
+# t_0 = 0.
+# y_0 = 4.
+# y_1 = 0
+# y_2 = 0
+# # Punto final del intervalo de solución
+# T = 5.
+# # Tamaño de paso
+# hp = 0.5
+
+# # Calculo del número de pasos (ó número de iteraciones)
+# nit = int((T - t_0)/hp)
+
+# # Solución con el método de Euler
+# # fld,fld2,t0,y0,y1,y2,n,h
+# t, y1,y2 = pvim.euler_meth_2(fld,fld2,t_0,y_1,y_2,nit,hp)
+# # the, yhe = pvim.heun_meth2(fld,fld2,t_0,y_0,y_1,y_2,nit,hp)
+# # tptom, yptom = pvim.ptomed_meth2(fld,fld2,t_0,y_0,y_1,y_2,nit,hp)
+
+# plt.plot(t, y1,'o', label='Method Euler')
+# plt.plot(t,y2,'-')
+
+# # teu, yeu = pvim.euler_meth(fld2,t_0,y_1,nit,hp)
+# # the, yhe = pvim.heun_meth(fld2,t_0,y_1,nit,hp)
+# # tptom, yptom = pvim.ptomed_meth(fld2,t_0,y_1,nit,hp)
+
+# # Valores de t para construir la gráfica de la solución analítica
+# t_v = np.linspace(t_0,T,100)
+# print(t,y2,'-')
+
+# # Gráfica con las soluciones obtenidas
+# plt.xlabel('t')
+# plt.ylabel('y(t)')
+# plt.legend()
+# plt.title('Soluciones numéricas del PVI planteado')
+# plt.show()
+
+
 '''
-Código construido para solucionar un PVIs conformado por una EDO de primer orden de la forma y'=fld(t,y) y condiciones iniciales y(t_0)=y_0.
+Modificación del código utilizado en la semana 4 para implementar los métodos de PVI en el método del disparo.
+En este código se soluciona un PVI de segundo orden conformado por dos EDOs de primer orden de la forma 
+y1'=fld1(t,y1,y2), y2'=fld2(t,y1,y2) y dos condiciones iniciales y1(t_0)=y1_0, y2(t_0)=y2_0.
 TAREA: Agregar líneas comentadas que faciliten la comprensión del código.
 '''
 
@@ -8,23 +70,24 @@ import pvi_methods as pvim
 import matplotlib.pyplot as plt
 
 # Definición de la función f (lado derecho de la EDO)
-def fld(t, y_1,y_2):
-    return y_2
+def fld1(t, y):
+    return y[1]
 
-def fld2(t, y_1, y_2):
-    return -0.6*y_2 -8*y_1
+def fld2(t, y):
+    return -0.6*y[1] - 8*y[0]
 
 # Solución analítica del PVI
 def fanalit(t):
-    y = np.exp(t)
+    y = np.exp(-0.3*t)*(4*np.cos(2.812*t) + 0.4267*np.sin(2.812*t))
     return y
 
 # Información de entrada
 # Condiciones iniciales
 t_0 = 0.
-y_0 = 4.
-y_1 = 0
-y_2 = 0
+y1_0 = 4.
+y2_0 = 0.
+y = [y1_0, y2_0]
+
 # Punto final del intervalo de solución
 T = 5.
 # Tamaño de paso
@@ -34,23 +97,14 @@ hp = 0.5
 nit = int((T - t_0)/hp)
 
 # Solución con el método de Euler
-# fld,fld2,t0,y0,y1,y2,n,h
-t, y1,y2 = pvim.euler_meth_2(fld,fld2,t_0,y_1,y_2,nit,hp)
-# the, yhe = pvim.heun_meth2(fld,fld2,t_0,y_0,y_1,y_2,nit,hp)
-# tptom, yptom = pvim.ptomed_meth2(fld,fld2,t_0,y_0,y_1,y_2,nit,hp)
-
-plt.plot(t, y1,'o', label='Method Euler')
-plt.plot(t,y2,'-')
-
-# teu, yeu = pvim.euler_meth(fld2,t_0,y_1,nit,hp)
-# the, yhe = pvim.heun_meth(fld2,t_0,y_1,nit,hp)
-# tptom, yptom = pvim.ptomed_meth(fld2,t_0,y_1,nit,hp)
+teu, yeu = pvim.euler_methord2(fld1, fld2, t_0, y, nit, hp)
 
 # Valores de t para construir la gráfica de la solución analítica
 t_v = np.linspace(t_0,T,100)
-print(t,y2,'-')
 
 # Gráfica con las soluciones obtenidas
+plt.plot(t_v, fanalit(t_v),label='Solución analítica')
+plt.plot(teu, yeu[0,:],'--',label='Método de Euler')
 plt.xlabel('t')
 plt.ylabel('y(t)')
 plt.legend()
--- Class10/pvi_methods.py
+++ Class10/pvi_methods.py
@@ -1,11 +1,67 @@
+# '''
+# TAREAS: 
+# 1. Escribir un encabezado para el módulo en el que se indiquen las funciones que contiene y el tipo de problemas que resuelven. Asegúrese de incluir el aspecto o los aspectos que tienen en común las funciones para ser incluidas en un mismo módulo.
+# 2. Agregar líneas comentadas que faciliten la comprensión del código.
+# 3. Agregar una función por cada método estudiado en clase y que no haya sido incluido en este módulo.
+# '''
+
+# # d^2y/dx^2 + 0.6 dy/dx + 8y = 0
+
+# import numpy as np
+
+# def euler_meth(fld,t0,y0,n,h):
+#     t = np.zeros(n+1)
+#     y = np.zeros(n+1)
+#     t[0] = t0
+#     y[0] = y0
+#     for i in range(n):
+#         y[i+1] = y[i] + h*fld(t[i], y[i])
+#         t[i+1] = t[i] + h
+#     return t,y
+
+# def heun_meth(fld,t0,y0,n,h):
+#     t = np.zeros(n+1)
+#     y = np.zeros(n+1)
+#     t[0] = t0
+#     y[0] = y0
+#     for i in range(n):
+#         yaux = y[i] + h*fld(t[i], y[i])
+#         t[i+1] = t[i] + h
+#         pend = (fld(t[i], y[i]) + fld(t[i+1], yaux))/2.0
+#         y[i+1] = y[i] + h*pend
+#     return t,y
+
+# def ptomed_meth(fld,t0,y0,n,h):
+#     t = np.zeros(n+1)
+#     y = np.zeros(n+1)
+#     t[0] = t0
+#     y[0] = y0
+#     for i in range(n):
+#         yaux = y[i] + h*fld(t[i], y[i])/2.0
+#         taux = t[i] + 0.5*h
+#         y[i+1] = y[i] + h*fld(taux, yaux)
+#         t[i+1] = t[i] + h
+#     return t,y
+
+# def euler_meth_2(fld,fld2,t0,y1,y2,n,h):
+#     t = np.zeros(n+1)
+#     y_1 = np.zeros(n+1,float)
+#     y_2 = np.zeros(n+1,float)
+#     t[0] = t0
+#     y_1[0] = y1
+#     y_2[0] = y2
+#     for i in range(n):
+#         y_1[i+1] = y_1[i] + h*fld(t[i], y_1[i],y_2[i])
+#         y_2[i+1] = y_2[i] + h*fld(t[i], y_1[i],y_2[i])
+#         t[i+1] = t[i] + h
+#     return t,y_1,y_2
+
 '''
 TAREAS: 
 1. Escribir un encabezado para el módulo en el que se indiquen las funciones que contiene y el tipo de problemas que resuelven. Asegúrese de incluir el aspecto o los aspectos que tienen en común las funciones para ser incluidas en un mismo módulo.
 2. Agregar líneas comentadas que faciliten la comprensión del código.
 3. Agregar una función por cada método estudiado en clase y que no haya sido incluido en este módulo.
 '''
-
-# d^2y/dx^2 + 0.6 dy/dx + 8y = 0
 
 import numpy as np
 
@@ -16,6 +72,18 @@ def euler_meth(fld,t0,y0,n,h):
     y[0] = y0
     for i in range(n):
         y[i+1] = y[i] + h*fld(t[i], y[i])
+        t[i+1] = t[i] + h
+    return t,y
+
+def euler_methord2(fld1, fld2, t0, yo2, n, h):
+    t = np.zeros(n+1)
+    y = np.zeros((2,n+1))
+    t[0] = t0
+    y[0,0] = yo2[0]
+    y[1,0] = yo2[1]
+    for i in range(n):
+        y[0,i+1] = y[0,i] + h*fld1(t[i], y[:,i])
+        y[1,i+1] = y[1,i] + h*fld2(t[i], y[:,i])
         t[i+1] = t[i] + h
     return t,y
 
@@ -42,16 +110,3 @@ def ptomed_meth(fld,t0,y0,n,h):
         y[i+1] = y[i] + h*fld(taux, yaux)
         t[i+1] = t[i] + h
     return t,y
-
-def euler_meth_2(fld,fld2,t0,y1,y2,n,h):
-    t = np.zeros(n+1)
-    y_1 = np.zeros(n+1,float)
-    y_2 = np.zeros(n+1,float)
-    t[0] = t0
-    y_1[0] = y1
-    y_2[0] = y2
-    for i in range(n):
-        y_1[i+1] = y_1[i] + h*fld(t[i], y_1[i],y_2[i])
-        y_2[i+1] = y_2[i] + h*fld(t[i], y_1[i],y_2[i])
-        t[i+1] = t[i] + h
-    return t,y_1,y_2
